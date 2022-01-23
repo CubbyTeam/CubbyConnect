ccf9a30e8cdc1fe70594ef05bd63c81835344ef6
Jaeyong Sung <jaeyong0201@gmail.com>
2022-01-23 18:58:19 +0900

    change to rust

--- client/Includes/Server.hpp
+++
@@ -1,6 +0,0 @@
-#ifndef CUBBY_CONNECT_CLIENT_SERVER_HPP
-#define CUBBY_CONNECT_CLIENT_SERVER_HPP
-
-int Add(int a, int b);
-
-#endif
--- client/Sources/Server.cpp
+++
@@ -1,6 +0,0 @@
-#include <Server.hpp>
-
-int Add(int a, int b)
-{
-    return a + b;
-} 
